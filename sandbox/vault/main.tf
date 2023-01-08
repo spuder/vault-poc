de1b84b374fbee6ced23d63095335a1b44a7a36f
Spencer Owen <owenspencer@gmail.com>
2023-01-07 19:02:22 -0700

    foo

--- sandbox/vault/main.tf
+++ sandbox/vault/main.tf
@@ -1,8 +1,15 @@
-provider "vault" {
+# provider "vault" {
 
-}
+# }
 
 terraform {
 
 }
 
+
+
+resource "null_resource" "foo" {
+  provisioner "local-exec" {
+    command = "echo 'foo' > foo.txt"
+  }
+}
